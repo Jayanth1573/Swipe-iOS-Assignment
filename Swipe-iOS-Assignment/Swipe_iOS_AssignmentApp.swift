8ade4886323f15ddf880351fdd2e213d57616996
Jayanth <jayanthambaldhage15@gmail.com>
2024-11-15 16:38:00 +0530

    Initial Commit

---
+++ Swipe-iOS-Assignment/Swipe_iOS_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swipe_iOS_AssignmentApp.swift
+//  Swipe-iOS-Assignment
+//
+//  Created by Jayanth Ambaldhage on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_iOS_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
